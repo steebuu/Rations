2f9bf5e742530fa2b6163bccd8c15c1215123302
Steve <T.AllGameMail@protonmail.com>
2019-12-30 13:04:12 -0500

    Added dependencies for redux, react-redux, react-router-dom and redux-logger

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import './navbar.css'
+// import './navbar.css'
 
 class NavBar extends React.Component {
     constructor(props) {
