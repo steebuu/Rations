1a2fe68765fe05007e204cf0b5bf800a991a30f3
Steve <T.AllGameMail@protonmail.com>
2019-12-30 12:13:44 -0500

    Adds authroutes for /login and /signup to app component

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,13 +1,22 @@
+
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
+import NavBarContainer from './nav/navbar_container';
 
 import MainPage from './main/main_page';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
-    <Switch>
-        <AuthRoute exact path="/" component={MainPage} />
-    </Switch>
+    <div>
+        <NavBarContainer />
+        <Switch>
+            <AuthRoute exact path="/" component={MainPage} />
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        </Switch>
+    </div>
 );
 
 export default App;
