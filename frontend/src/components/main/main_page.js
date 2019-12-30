70a8ac9fec82dd5bd972b5ea2187bbada941f6e2
Steve <T.AllGameMail@protonmail.com>
2019-12-30 12:12:01 -0500

    Adds main page component

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>A Twitter Clone</h1>
+                <footer>
+                    Copyright &copy; 2019 Chirper
+        </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
