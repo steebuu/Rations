ad0d41aefa08386266b67cf76019350adb70f785
Steve <T.AllGameMail@protonmail.com>
2019-12-30 12:03:50 -0500

    Deletes src folder, creates user logout action, creates new entry file

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,22 @@
+// src/actions/session_actions.js
+
+// Although there's only one function here so far, let's import the whole file since we will be adding more later
+import * as APIUtil from '../util/session_api_util';
+import jwt_decode from 'jwt-decode';
+
+// This pattern should be familiar to you from the full stack project
+
+export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+
+export const logoutUser = () => ({
+    type: RECEIVE_USER_LOGOUT
+});
+
+export const logout = () => dispatch => {
+    // Remove the token from local storage
+    localStorage.removeItem('jwtToken')
+    // Remove the token from the common axios header
+    APIUtil.setAuthToken(false)
+    // Dispatch a logout action
+    dispatch(logoutUser())
+};
