83eb94a163f3e96212b177a1e42c09723f0f07b2
Steve <T.AllGameMail@protonmail.com>
2019-12-30 12:07:36 -0500

    Adds root reducer

---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+// src/reducers/root_reducer.js
+
+import { combineReducers } from 'redux';
+import session from './session_reducer';
+
+const RootReducer = combineReducers({
+    session
+});
+
+export default RootReducer;
