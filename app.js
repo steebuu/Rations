045a425726179bf8e2fa5c49d74c490e50d7f712
Steve <T.AllGameMail@protonmail.com>
2019-12-30 13:47:06 -0500

    merges master into frontend auth

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const users = require("./routes/api/users");
 const recipes = require("./routes/api/recipes");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const express = require("express");
 const app = express();
@@ -17,6 +18,9 @@ app.get("/", (req, res) => {
   console.log(res);
   res.send("Greetings World");
 });
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,17 +2,25 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.post('/register', (req, res) => {
-  debugger;
-  // Check to make sure nobody has already registered with a duplicate email
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email })
     .then(user => {
       if (user) {
-        // Throw a 400 error if the email address already exists
-        return res.status(400).json({ email: "A user has already registered with this address" })
+        return res.status(400).json({ email: "A user has already registered with this address" });
       } else {
-        // Otherwise create a new user
         const newUser = new User({
           username: req.body.username,
           email: req.body.email,
@@ -30,6 +38,52 @@ router.post('/register', (req, res) => {
         });
       }
     });
+});
+
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ email: 'This user does not exist' });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = { id: user.id, handle: user.handle };
+
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              // Tell the key to expire in one hour
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
+        });
+    });
+});
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    username: req.user.username,
+    email: req.user.email
+  });
 });
 
 
