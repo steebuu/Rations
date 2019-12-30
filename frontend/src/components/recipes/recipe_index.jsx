3aa6cc6916e4a1589b2c823f26761dd8df38145f
Steve <T.AllGameMail@protonmail.com>
2019-12-30 15:17:57 -0500

    Adds recipe index component/container and recipe index item component/container

---
+++ frontend/src/components/recipes/recipe_index.jsx
@@ -0,0 +1,22 @@
+//a list of recipe index items scraped from api and database
+
+import React, { Component } from 'react'
+import RecipeIndexItem from './recipe_index_item';
+
+export default class RecipeIndex extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount(){
+        
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ frontend/src/components/recipes/recipe_index_item.jsx
@@ -0,0 +1,13 @@
+//an atomic recipe index item containing light information (e.g image, title, author) about the recipe
+
+import React, { Component } from 'react'
+
+export default class RecipeIndexItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
