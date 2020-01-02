63e14ff89000619c3ee7fcdba1b2a15c1d3b9c9c
Steve <T.AllGameMail@protonmail.com>
2020-01-02 14:35:56 -0500

    Adds a form to comments form

--- frontend/src/components/comments/comments_form.jsx
+++ frontend/src/components/comments/comments_form.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 
 export default class CommentsForm extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            body: "",
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        const {user, recipeId, createComment} = this.props;
+        const {body} = this.state;
+        const data = {user: user.id, recipe: recipeId, body: body}
+
+        createComment(data);
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Add a comment..." id=""/>
+                <button>Submit Comment</button>
+            </form>
         )
     }
 }
