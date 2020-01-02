48a77e9a847217cd34a369719d9a292f20e6c608
Steve <T.AllGameMail@protonmail.com>
2020-01-02 14:28:16 -0500

    Creates comments form and comments form container for comment creation/deletion/edits

---
+++ frontend/src/components/comments/comments_form.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CommentsForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
