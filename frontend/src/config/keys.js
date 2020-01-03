e43a803d492b749d8bb094cd49ba6d187ab4f64c
Valerie Whitehouse <vawhitehouse@yahoo.com>
2020-01-02 20:57:55 -0500

    removed keys.js

--- frontend/src/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://DEV:A0FvVvhAWElfQFQ6@cluster0-cmcbe.mongodb.net/rationsDB?retryWrites=true&w=majority',
-  secretOrKey: 'Kbyme?A)I[/p!f>',
-  recipeKey: '26f06f50f12348bca27a51660ba27741'
-};
