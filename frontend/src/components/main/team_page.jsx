0fdc7f65e1e47332f5eea61a3cf44ab656562b8e
Steve <T.AllGameMail@protonmail.com>
2020-01-29 12:32:49 -0500

    adds profile pictures to team page

--- frontend/src/components/main/team_page.jsx
+++ frontend/src/components/main/team_page.jsx
@@ -5,33 +5,42 @@ export default class TeamPage extends Component {
     render() {
         return (
             <div className="team-div">
-                <div>
+                <div className="member-div">
+                    <div className="prof-pic-holder">
+                        <img className="prof-pic" src="ryan_pic.jpeg"></img>
+                    </div>
                     <h2>Ryan Monahan</h2>
-                    <a href="https://github.com/Rmonahan"><i className="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/ryan-monahan-99323b184/"><i className="fab fa-linkedin"></i></a>
-                    <a href="https://angel.co/ryan-monahan-2"><i className="fab fa-angellist"></i></a>
-                    <a href="https://rmonahan.github.io/"><i className="fas fa-suitcase"></i></a>
+                    <a className="team-link" href="https://github.com/Rmonahan"><i className="fab team-icon fa-github"></i></a>
+                    <a className="team-link" href="https://www.linkedin.com/in/ryan-monahan-99323b184/"><i className="fab team-icon fa-linkedin"></i></a>
+                    <a className="team-link" href="https://angel.co/ryan-monahan-2"><i className="fab team-icon fa-angellist"></i></a>
+                    <a className="team-link" href="https://rmonahan.github.io/"><i className="fas team-icon fa-suitcase"></i></a>
                 </div>
-                <div>
+                <div className="member-div">
+                    <div className="prof-pic-holder">
+                        <img className="prof-pic" src="shree_pic.jpeg"></img>
+                    </div>
                     <h2>Shree Patel</h2>
-                    <a href="https://github.com/ShreePatel95"><i className="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/shree1795/"><i className="fab fa-linkedin"></i></a>
-                    <a href="https://angel.co/shree-patel"><i className="fab fa-angellist"></i></a>
-                    <a href="https://www.shreejpatel.com/"><i className="fas fa-suitcase"></i></a>
+                    <a className="team-link" href="https://github.com/ShreePatel95"><i className="fab team-icon fa-github"></i></a>
+                    <a className="team-link" href="https://www.linkedin.com/in/shree1795/"><i className="fab team-icon fa-linkedin"></i></a>
+                    <a className="team-link" href="https://angel.co/shree-patel"><i className="fab team-icon fa-angellist"></i></a>
+                    <a className="team-link" href="https://www.shreejpatel.com/"><i className="fas team-icon fa-suitcase"></i></a>
                 </div>
-                <div>
+                <div className="member-div">
+                    <div className="prof-pic-holder">
+                        <img className="prof-pic" src="steve_pic.jpeg"></img>
+                    </div>
                     <h2>Steve Liu</h2>
-                    <a href="https://github.com/steebuu"><i className="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/liu-steve/"><i className="fab fa-linkedin"></i></a>
-                    <a href="https://angel.co/steve-liu-8"><i className="fab fa-angellist"></i></a>
-                    <a href=""><i className="fas fa-suitcase"></i></a>
+                    <a className="team-link" href="https://github.com/steebuu"><i className="fab team-icon fa-github"></i></a>
+                    <a className="team-link" href="https://www.linkedin.com/in/liu-steve/"><i className="fab team-icon fa-linkedin"></i></a>
+                    <a className="team-link" href="https://angel.co/steve-liu-8"><i className="fab team-icon fa-angellist"></i></a>
+                    <a className="team-link" href=""><i className="fas team-icon fa-suitcase"></i></a>
                 </div>
-                <div>
+                <div className="member-div">
                     <h2>Valerie Whitehouse</h2>
-                    <a href="https://github.com/vawhitehouse"><i className="fab fa-github"></i></a>
-                    <a href="https://www.linkedin.com/in/valeriewhitehouse/"><i className="fab fa-linkedin"></i></a>
-                    <a href="https://angel.co/valerie-whitehouse"><i className="fab fa-angellist"></i></a>
-                    <a href=""><i className="fas fa-suitcase"></i></a>
+                    <a className="team-link" href="https://github.com/vawhitehouse"><i className="fab team-icon fa-github"></i></a>
+                    <a className="team-link" href="https://www.linkedin.com/in/valeriewhitehouse/"><i className="fab team-icon fa-linkedin"></i></a>
+                    <a className="team-link" href="https://angel.co/valerie-whitehouse"><i className="fab team-icon fa-angellist"></i></a>
+                    <a className="team-link" href=""><i className="fas team-icon fa-suitcase"></i></a>
                 </div>
             </div>
         )
