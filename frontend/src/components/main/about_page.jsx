8dfc26396a3f5445d13352a9af808ad0ac4eed60
Steve <T.AllGameMail@protonmail.com>
2020-01-29 15:27:12 -0500

    adds basic styling to about page

--- frontend/src/components/main/about_page.jsx
+++ frontend/src/components/main/about_page.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './about.css';
 
 export default function aboutPage() {
     return (
-        <div>
-            <h1>Welcome to Rations!</h1>
+        <div className="about-container">
+            <h1 className="about-welcome">Welcome to Rations!</h1>
             <p> Rations is a recipe finding app - input ingredients you have lying around to see what you can make! </p>
             <p> Search by entering ingredient names separated by commas i.e "bread, eggs" </p>
-            <p> Liking a recipe adds it to your favorites - check out Favorites to see what recipes you've liked and click to see that recipe. </p>
-            <p> Comment on recipes to let others know how you feel. </p>
+            <p> Liking a recipe adds it to your favorites - check out Favorites to see what recipes you've liked and click to see that recipe </p>
+            <p> Comment on recipes to let others know how you feel </p>
         </div>
     )
 }
