503d60b86971c91800d23e85d5916da937d20e6a
Steve <T.AllGameMail@protonmail.com>
2020-01-29 11:56:03 -0500

    adds favicons for links to sites

--- frontend/src/components/main/team_page.jsx
+++ frontend/src/components/main/team_page.jsx
@@ -1,36 +1,37 @@
 import React, { Component } from 'react'
+import './team.css';
 
 export default class TeamPage extends Component {
     render() {
         return (
-            <div>
+            <div className="team-div">
                 <div>
                     <h2>Ryan Monahan</h2>
-                    <a href="https://github.com/Rmonahan">Github</a>
-                    <a href="https://www.linkedin.com/in/ryan-monahan-99323b184/">LinkedIn</a>
-                    <a href="https://angel.co/ryan-monahan-2">AngelList</a>
-                    <a href="https://rmonahan.github.io/">Portfolio</a>
+                    <a href="https://github.com/Rmonahan"><i className="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/ryan-monahan-99323b184/"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://angel.co/ryan-monahan-2"><i className="fab fa-angellist"></i></a>
+                    <a href="https://rmonahan.github.io/"><i className="fas fa-suitcase"></i></a>
                 </div>
                 <div>
                     <h2>Shree Patel</h2>
-                    <a href="https://github.com/ShreePatel95">Github</a>
-                    <a href="https://www.linkedin.com/in/shree1795/">LinkedIn</a>
-                    <a href="https://angel.co/shree-patel">AngelList</a>
-                    <a href="https://www.shreejpatel.com/">Portfolio</a>
+                    <a href="https://github.com/ShreePatel95"><i className="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/shree1795/"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://angel.co/shree-patel"><i className="fab fa-angellist"></i></a>
+                    <a href="https://www.shreejpatel.com/"><i className="fas fa-suitcase"></i></a>
                 </div>
                 <div>
                     <h2>Steve Liu</h2>
-                    <a href="https://github.com/steebuu">Github</a>
-                    <a href="https://www.linkedin.com/in/liu-steve/">LinkedIn</a>
-                    <a href="https://angel.co/steve-liu-8">AngelList</a>
-                    <a href="">Portfolio</a>
+                    <a href="https://github.com/steebuu"><i className="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/liu-steve/"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://angel.co/steve-liu-8"><i className="fab fa-angellist"></i></a>
+                    <a href=""><i className="fas fa-suitcase"></i></a>
                 </div>
                 <div>
                     <h2>Valerie Whitehouse</h2>
-                    <a href="https://github.com/vawhitehouse">Github</a>
-                    <a href="https://www.linkedin.com/in/valeriewhitehouse/">LinkedIn</a>
-                    <a href="https://angel.co/valerie-whitehouse">AngelList</a>
-                    <a href="">Portfolio</a>
+                    <a href="https://github.com/vawhitehouse"><i className="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/valeriewhitehouse/"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://angel.co/valerie-whitehouse"><i className="fab fa-angellist"></i></a>
+                    <a href=""><i className="fas fa-suitcase"></i></a>
                 </div>
             </div>
         )
