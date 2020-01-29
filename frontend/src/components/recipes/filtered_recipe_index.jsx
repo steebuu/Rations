f95f5ed0ce7ec55b57f7866f1fa812394cd5c9da
Steve <T.AllGameMail@protonmail.com>
2020-01-29 10:51:41 -0500

    Adds hyperlink for recipe source, styles it, and adds no results found for no search results

--- frontend/src/components/recipes/filtered_recipe_index.jsx
+++ frontend/src/components/recipes/filtered_recipe_index.jsx
@@ -11,6 +11,8 @@ export default class FilteredRecipeIndex extends Component {
         const { recipes } = this.props;
         if (!recipes) {
             return null;
+        } else if( Object.keys(recipes).length === 0) {
+            return <p className="no-results-found">No results found! :(</p>
         }
         const recipeLis = Object.values(recipes).map(recipe => {
             return <RecipeIndexItem recipe={recipe} key={recipe.id} />
